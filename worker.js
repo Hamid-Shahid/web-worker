3d2b2daa2cd6f5319af6daa575f4435596b99189
Hammad-Ejaz <hamidshahid599@gmail.com>
2024-02-06 21:46:31 +0500

    Update worker.js
    update loop count

--- worker.js
+++ worker.js
@@ -1,7 +1,7 @@
 self.onmessage = function(message)
 {
     let sum = 0;
-    for (let i=0; i < 10000; i++){
+    for (let i=0; i < 10000000; i++){
         sum += 1;
     }
     self.postMessage(sum);
