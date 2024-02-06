6bc2fb794104f50e6af8a71e96d2cdc0e40cd3ac
Hammad-Ejaz <hamidshahid599@gmail.com>
2024-02-06 22:30:35 +0500

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,7 +1,7 @@
 self.onmessage = function(message)
 {
     let sum = 0;
-    for (let i=0; i < 100000000; i++){
+    for (let i=0; i < 1000000; i++){
         sum += 1;
     }
     self.postMessage(sum);
